1827d424184f0c1b990b1faf676445b31f6f5a4b
AswiN2064 <nagamurali2064@gmail.com>
2023-08-04 15:54:45 +0530

    commit1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://127.0.0.1:5500/"
           target="_blank"
           rel="noopener noreferrer"
         >
